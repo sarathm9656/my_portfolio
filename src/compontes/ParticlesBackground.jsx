db4d3d7fec2ea50e6e1f8376068e9904b94a90d3
Sarath M <sarathmullath9656@gmail.com>
2025-01-18 09:35:48 +0530

    first commit

---
+++ src/compontes/ParticlesBackground.jsx
@@ -0,0 +1,94 @@
+import React, { useCallback } from "react";
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim"; 
+import "./partiles.css"; 
+
+export default function Particles_back() {
+  const particlesInit = useCallback(async (engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    console.log(container); 
+  }, []);
+
+  return (
+    <div className="particles-wrapper">
+     
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "black",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: false,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "none",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.8,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 4,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 6 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+}
